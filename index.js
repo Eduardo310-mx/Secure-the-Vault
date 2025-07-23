2585d24c8690594a6aa6d098564c319057b519ad
Eduardo Maldonado <eduardo.maldonado.lop@gmail.com>
2025-07-23 10:37:00 -0600

    Add a Codeb inside the last line of console

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var code1
-var code1 = 10
-console.log(code1 + 3);
+var code1 = 7 + 3
+console.log(code1);
 
-var code2
 var code2 = 100 / 2 - 10
+console.log(code2)
 
 var code3
 var code3 = 40 - (1 % 2)
-console.log("The vault has been secured. The combination is: " + code3)
+console.log(code3)
 
 var message = "The vault has been secured. The combination is: "
 
